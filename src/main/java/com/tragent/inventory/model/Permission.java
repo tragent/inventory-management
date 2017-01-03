7b6f2c13ca2893bdeac23e202219a890f56556bb
Anh3h <courageangeh@gmail.com>
2017-01-03 17:29:32 +0100

    #inv: Implemented models for roles and permission and added new attribute(role) to user model

---
+++ src/main/java/com/tragent/inventory/model/Permission.java
@@ -0,0 +1,51 @@
+package com.tragent.inventory.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Permission {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false)
+	private String name;
+	private String description;
+	
+	@OneToMany()
+	private List<Role> role;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
