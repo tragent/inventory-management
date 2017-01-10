16cf43a51d149c08daf97b66d797e619168fe9d0
Elvine Egoko <leaelvine@gmail.com>
2017-01-10 05:06:48 +0100

    #inv: created product endpoint

---
+++ src/main/java/com/tragent/inventory/service/ProductService.java
@@ -0,0 +1,55 @@
+package com.tragent.inventory.service;
+
+import java.util.Collection;
+import com.tragent.inventory.model.Product;
+
+/* Service that provides CRUD operation for Products  */
+public interface ProductService {
+	
+	/**
+	 * Get all products in the system.
+	 * 
+	 * @return collection of all product in the system
+	 */
+	public Collection<Product> findAll();
+	
+	/**
+	 * Find a product by Id.
+	 * 
+	 * @param id the product's id
+	 * @return the product object if found else return null
+	 */
+	public Product findById(Long id);
+	
+	/**
+	 * Find a product by name.
+	 * 
+	 * @param name the product's name
+	 * @return the product object if found else return null
+	 */
+	public Product findByName(String name);
+	
+	/**
+	 * Create new Product.
+	 * 
+	 * @param  the product to be created
+	 * @return the created product 
+	 */
+	public Product create(Product product);
+	
+	/**
+	 * Update an existing product's information.
+	 * 
+	 * @param product the updated product's record
+	 * @return the updated product object
+	 */
+	public Product update(Product product);
+	
+	/**
+	 * Delete a product from the system.
+	 * 
+	 * @param id product's id
+	 */
+	public void delete(Long id);
+
+}
---
+++ src/main/java/com/tragent/inventory/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,76 @@
+package com.tragent.inventory.service.implementation;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tragent.inventory.model.Customer;
+import com.tragent.inventory.model.Product;
+import com.tragent.inventory.repository.ProductRepository;
+import com.tragent.inventory.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	public Collection<Product> findAll() {
+		Collection<Product> product = productRepository.findByIsActive(true);
+		return product;
+	}
+
+	@Override
+	public Product findById(Long id) {
+		Product product = productRepository.findOne(id);
+		return product;
+	}
+
+	@Override
+	public Product findByName(String name) {
+		Product product = productRepository.findByName(name);
+		return product;
+	}
+
+	@Override
+	public Product create(Product product) {
+		
+		if (product.getName() == null) {
+			//cannot create product without a name
+			return null;
+		}
+		
+		productRepository.save(product);
+		return product;
+	}
+
+	@Override
+	public Product update(Product product) {
+		Product productPersisted = findById(product.getId());
+		if (productPersisted == null) {
+			//cannot update product without a category name
+			return null;
+		}
+		
+		productRepository.save(product);
+		return product;
+	}
+
+	@Override
+	public void delete(Long id) {
+		Product product = findById(id);
+		if (product == null) {
+			return;
+		}
+		if(product.isActive() == true){
+			product.setIsActive(false);
+		}
+		else{
+			product.setIsActive(true);
+		}
+		productRepository.save(product);
+		
+	}
+
+}
